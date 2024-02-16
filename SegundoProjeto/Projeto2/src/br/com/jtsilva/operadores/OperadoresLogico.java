b920efa0674fabc1b7947dad4d3d6623b769d76f
jtigik <jeftigik@hotmail.com>
2024-02-16 02:02:50 -0300

    Refinamento do teste

--- SegundoProjeto/Projeto2/src/br/com/jtsilva/operadores/OperadoresLogico.java
+++ SegundoProjeto/Projeto2/src/br/com/jtsilva/operadores/OperadoresLogico.java
@@ -10,17 +10,10 @@ public class OperadoresLogico {
         // 2 - rendaMensal >= 2000
         // 3 - temEmprego
 
-        boolean avaliaIdade = idade >= 18 && idade <= 35;
-        boolean avaliaRenda = rendaMensal >= 2000;
-
-        System.out.println(avaliaIdade);
-        System.out.println(avaliaRenda);
-
-        if (avaliaIdade && avaliaRenda && temEmprego) {
+        if (idade >= 18 && idade <= 35 && rendaMensal >= 2000 && temEmprego) {
             System.out.println("Pode fazer empréstimo!");
         } else {
             System.out.println("Não pode fazer empréstimo!");
         }
-
     }
 }
