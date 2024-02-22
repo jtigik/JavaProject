ca21130c36e719b6a28590860040d1d1f67ea875
jtigik <jeftigik@hotmail.com>
2024-02-22 03:39:53 -0300

    Add new features

---
+++ SegundoProjeto/Projeto2/src/br/com/jtsilva/introduction/TesteLoop.java
@@ -0,0 +1,17 @@
+package io.github.jiangdequan;
+
+public class TesteLoop {
+    public static void main(String[] args) {
+
+        int num = 0;
+
+        looping(num);
+
+    }
+
+    public static void looping(int num) {
+        while (num < 10) {
+            System.out.printf("Testing %d\n", num++);
+        }
+    }
+}
---
+++ SpringBootPrimeiroExemplo/src/main/java/br/com/jtsilva/Test.java
@@ -0,0 +1,7 @@
+package br.com.jtsilva;
+
+public interface Test {
+
+    boolean verificaObj(Object objeto);
+
+}
