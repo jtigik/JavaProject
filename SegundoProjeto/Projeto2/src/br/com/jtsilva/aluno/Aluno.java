9628a962f17226a76386391f680120fddddcd99b
jtigik <jeftigik@hotmail.com>
2024-02-26 17:00:47 -0300

    Add Class X Object concept

---
+++ SegundoProjeto/Projeto2/src/br/com/jtsilva/aluno/Aluno.java
@@ -0,0 +1,6 @@
+package br.com.jtsilva.aluno;
+
+public class Aluno {
+    int id; // Variável de instância
+    String nome; // Variável de instância
+}
---
+++ SegundoProjeto/Projeto2/src/br/com/jtsilva/aluno/Exemplo.java
@@ -0,0 +1,14 @@
+package br.com.jtsilva.aluno;
+
+import br.com.jtsilva.aluno.Aluno;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno(); // Criando um objeto da classe Aluno
+        aluno1.id = 1;
+        aluno1.nome = "Maria";
+
+        System.out.println("ID do aluno: " + aluno1.id);
+        System.out.println("Nome do aluno: " + aluno1.nome);
+    }
+}
