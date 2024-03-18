d2e2c3851eea3c30d2912e39a7dff8dad7b90fc6
jtigik <jeftigik@hotmail.com>
2024-03-18 01:50:32 -0300

    Add Extends classes

---
+++ TiposPrimitivos/TiposPrimitivos/src/br/com/jtsilva/Arrays/ClasseTeste.java
@@ -0,0 +1,14 @@
+package br.com.jtsilva.Arrays;
+
+public class ClasseTeste extends StudentRecord {
+    public ClasseTeste(String studentName, double[] testScores) {
+        super(studentName, testScores);
+    }
+
+    public static void main(String[] args) {
+        double[] scores = { 85.5, 90.0, 78.5, 92.5 };
+        StudentRecord student1 = new StudentRecord("Alice", scores);
+        student1.displayStudentInfo();
+    }
+
+}
