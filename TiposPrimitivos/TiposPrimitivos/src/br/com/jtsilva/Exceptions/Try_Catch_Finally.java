0c1765394bdcaa09934ec2646346c81905ce5600
jtigik <jeftigik@hotmail.com>
2024-03-18 00:16:24 -0300

    Try_Catch_Finally updated

--- TiposPrimitivos/TiposPrimitivos/src/br/com/jtsilva/Exceptions/Try_Catch_Finally.java
+++ TiposPrimitivos/TiposPrimitivos/src/br/com/jtsilva/Exceptions/Try_Catch_Finally.java
@@ -3,13 +3,20 @@ package br.com.jtsilva.Exceptions;
 public class Try_Catch_Finally {
 
     public static void main(String[] args) {
-        try {
 
+        int tamanho = 0;
+
+        try {
             String nome = null;
-            int tamanho = nome.length();
+            tamanho = nome.length();
 
         } catch (IllegalArgumentException | NullPointerException e) {
             System.out.println("Caught exception: " + e.getMessage());
+
+        } finally {
+            System.out.println("Result: " + tamanho);
+
         }
+
     }
 }
