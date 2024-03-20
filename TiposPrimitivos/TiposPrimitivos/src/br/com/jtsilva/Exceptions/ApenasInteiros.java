de555dae55e88df7521287a5ed2c83315d587ed4
jtigik <jeftigik@hotmail.com>
2024-03-20 02:30:49 -0300

    Files update: apenasInteirios e LeitorDeArquivo

--- TiposPrimitivos/TiposPrimitivos/src/br/com/jtsilva/Exceptions/ApenasInteiros.java
+++ TiposPrimitivos/TiposPrimitivos/src/br/com/jtsilva/Exceptions/ApenasInteiros.java
@@ -1,12 +1,15 @@
 
 /*### Exercício para Iniciante: Tratamento de Entrada de Dados
 
-**Objetivo:** Criar uma classe que solicita ao usuário que insira um número inteiro. Se o usuário inserir um valor que não seja um número inteiro, o programa deve capturar a exceção e solicitar que ele tente novamente.
+**Objetivo:** Criar uma classe que solicita ao usuário que insira um número inteiro. Se o usuário
+inserir um valor que nãoseja um número inteiro, o programa deve capturar a exceção e solicitar
+que ele tente novamente.
 
 **Descrição:**
 1. Crie uma classe chamada `NumeroInteiro`.
 2. Dentro da classe, crie um método chamado `solicitarNumero` que lê um número do console.
-3. Utilize a estrutura Try-Catch para capturar qualquer `InputMismatchException` que possa surgir se o usuário inserir dados que não são um número inteiro.
+3. Utilize a estrutura Try-Catch para capturar qualquer `InputMismatchException` que possa surgir
+se o usuário inserir dados que não são um número inteiro.
 4. No bloco `finally`, coloque um print que sempre será executado, indicando o fim da execução do método.
 
 **Código:** */
--- TiposPrimitivos/TiposPrimitivos/src/br/com/jtsilva/Exceptions/LeitorDeArquivo.java
+++ TiposPrimitivos/TiposPrimitivos/src/br/com/jtsilva/Exceptions/LeitorDeArquivo.java
@@ -1,17 +1,18 @@
 /**
  * ### Exercício de Dificuldade Média: Manipulação de Arquivos
 
-**Objetivo:** Criar uma classe que lê um arquivo de texto e imprime seu conteúdo na tela. Se o arquivo não existir, o programa deve informar o usuário e criar um arquivo vazio com o nome solicitado.
+**Objetivo:** Criar uma classe que lê um arquivo de texto e imprime seu conteúdo na tela. Se o arquivo não existir,
+o programa deve informar o usuário e criar um arquivo vazio com o nome solicitado.
 
 **Descrição:**
 1. Crie uma classe chamada `LeitorDeArquivo`.
 2. Dentro da classe, crie um método chamado `lerArquivo` que recebe o nome de um arquivo como argumento.
-3. Utilize a estrutura Try-Catch para capturar uma `FileNotFoundException` e informar ao usuário que o arquivo não foi encontrado.
+3. Utilize a estrutura Try-Catch para capturar uma `FileNotFoundException` e informar ao usuário que o arquivo não
+foi encontrado.
 4. No bloco `finally`, verifique se o objeto `Scanner` foi criado e, se sim, feche-o.
 5. Se o arquivo não existir, crie um novo arquivo vazio com o nome fornecido.
 
  */
-
 package br.com.jtsilva.Exceptions;
 
 import java.io.File;
