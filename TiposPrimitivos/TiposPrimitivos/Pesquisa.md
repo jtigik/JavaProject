b76909bb03098afeabdafbf894251050333a6cd3
jtigik <jeftigik@hotmail.com>
2024-04-10 16:50:03 -0300

    Add new Set simulator

---
+++ TiposPrimitivos/TiposPrimitivos/Pesquisa.md
@@ -0,0 +1,17 @@
+# Exercício de Pesquisa: Explorando Diagramas de Classes e Casos de Uso
+
+## Objetivo
+Compreender e aplicar os conceitos de Diagramas de Classes e Diagramas de Casos de Uso na modelagem de sistemas orientados a objetos.
+
+## Tarefas
+
+1. **Pesquisa:** Pesquise sobre Diagramas de Classes e Diagramas de Casos de Uso. Anote as definições, propósitos, componentes e como eles são usados na modelagem de sistemas orientados a objetos.
+
+2. **Diagrama de Classes:** Imagine um sistema de gerenciamento de biblioteca. Crie um Diagrama de Classes para este sistema. Seu diagrama deve incluir classes como `Livro`, `Autor`, `Usuário`, `Emprestimo`, etc. Lembre-se de incluir atributos, métodos e relacionamentos adequados.
+
+3. **Diagrama de Casos de Uso:** Agora, crie um Diagrama de Casos de Uso para o mesmo sistema de gerenciamento de biblioteca. Identifique os atores (por exemplo, `Bibliotecário`, `Usuário`) e os casos de uso (por exemplo, `Empréstimo de Livro`, `Devolução de Livro`, `Cadastro de Novo Livro`).
+
+4. **Análise:** Analise como os Diagramas de Classes e Casos de Uso se complementam e contribuem para uma compreensão completa do sistema.
+
+## Entrega
+Prepare um relatório documentando suas descobertas da pesquisa, juntamente com os diagramas criados e sua análise. Certifique-se de que seus diagramas sejam claros e bem organizados.
---
+++ TiposPrimitivos/TiposPrimitivos/src/br/com/jtsilva/set/ExemploHashSetAluno.java
@@ -0,0 +1,71 @@
+package br.com.jtsilva.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExemploHashSetAluno {
+    public static void main(String args[]) {
+        exemploListaSimples();
+        exemploConsultando();
+        exemploRemover();
+    }
+
+    private static void exemploRemover() {
+        System.out.println("\n**** exemploRemover ****");
+        Set<Aluno> conjunto = new HashSet<Aluno>();
+
+        Aluno a = new Aluno("João da Silva", "Linux básico", 0);
+        Aluno b = new Aluno("Antonio Sousa", "OpenOffice", 0);
+        Aluno c = new Aluno("Lúcia Ferreira", "Internet", 0);
+        Aluno d = new Aluno("Antonio Sousa", "OpenOffice", 10);
+        Aluno e = new Aluno("Teste", "OpenOffice", 10);
+        conjunto.add(a);
+        conjunto.add(b);
+        conjunto.add(c);
+        conjunto.add(d);
+        System.out.println(conjunto);
+
+        conjunto.remove(a);
+        System.out.println(conjunto);
+
+        for (Aluno aluno : conjunto) {
+            System.out.println(aluno);
+        }
+    }
+
+    private static void exemploConsultando() {
+        System.out.println("\n**** exemploConsultando ****");
+        Set<Aluno> conjunto = new HashSet<Aluno>();
+
+        Aluno a = new Aluno("João da Silva", "Linux básico", 0);
+        Aluno b = new Aluno("Antonio Sousa", "OpenOffice", 0);
+        Aluno c = new Aluno("Lúcia Ferreira", "Internet", 0);
+        Aluno d = new Aluno("Antonio Sousa", "OpenOffice", 10);
+        Aluno e = new Aluno("Teste", "OpenOffice", 10);
+        conjunto.add(a);
+        conjunto.add(b);
+        conjunto.add(c);
+        conjunto.add(d);
+
+        System.out.println(conjunto.contains(e));
+    }
+
+    /**
+     * Para não repetir objetos na lista de alunos é necessário que a classe
+     * Aluno implemente o equals e hashcode
+     */
+    private static void exemploListaSimples() {
+        System.out.println("\n**** exemploListaSimples ****");
+        Set<Aluno> conjunto = new HashSet<Aluno>();
+
+        Aluno a = new Aluno("João da Silva", "Linux básico", 0);
+        Aluno b = new Aluno("Antonio Sousa", "OpenOffice", 0);
+        Aluno c = new Aluno("Lúcia Ferreira", "Internet", 0);
+        Aluno d = new Aluno("Antonio Sousa", "OpenOffice", 10);
+        conjunto.add(a);
+        conjunto.add(b);
+        conjunto.add(c);
+        conjunto.add(d);
+        System.out.println(conjunto);
+    }
+}
