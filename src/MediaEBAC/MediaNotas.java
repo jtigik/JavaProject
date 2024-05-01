1bf86f6453f96fb4e0e49ab2645ac3a89fefbc05
jtigik <jeftigik@hotmail.com>
2024-04-30 21:20:50 -0300

    AVG Class

---
+++ src/MediaEBAC/MediaNotas.java
@@ -0,0 +1,32 @@
+package MediaEBAC;
+
+import java.util.Scanner;
+
+public class MediaNotas {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println();
+
+        System.out.println("Digite a primeira nota:");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota:");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota:");
+        double nota3 = scanner.nextDouble();
+
+        System.out.println("Digite a quarta nota:");
+        double nota4 = scanner.nextDouble();
+
+        // Calcula a média das notas
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A média das 4 notas é: " + media);
+
+        scanner.close();
+    }
+}
